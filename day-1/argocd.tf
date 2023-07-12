c33983a75a2b9fb7d55815c7a27256a4b008ab23
Martins Eglitis <sitilge@gmail.com>
2023-07-12 10:19:06 +0300

    Increase argocd resource quotas

--- day-1/argocd.tf
+++ day-1/argocd.tf
@@ -43,9 +43,9 @@ resource "kubernetes_resource_quota" "argocd" {
 
   spec {
     hard = {
-      "limits.cpu"       = "1000m"
+      "limits.cpu"       = "1500m"
       "limits.memory"    = "2Gi"
-      "requests.cpu"     = "1000m"
+      "requests.cpu"     = "1500m"
       "requests.memory"  = "2Gi"
       "requests.storage" = "10Gi"
     }
